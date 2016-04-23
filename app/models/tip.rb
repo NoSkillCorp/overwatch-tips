2f9bdca3256ec67b081fdb9a12f563f6cba1851e
Caillou <igorlacroute@gmail.com>
2016-04-23 20:51:22 +0000

    suppression du title + raffinnage du js

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -4,7 +4,7 @@ class Tip < ActiveRecord::Base
     belongs_to :gaming_object
     
     validates :category, inclusion: { in: CATEGORIES, message: "%{value} is not a valid category" }
-    validates :title, :description, presence: true
+    validates :description, presence: true
     
     def self.empty_categories_hash
        CATEGORIES.map{ |category| [category, []] }.to_h
