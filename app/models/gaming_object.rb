76d7e6d984dd09702b14925a255bdc561b5a2bb4
Caillou <igorlacroute@gmail.com>
2016-04-23 12:44:30 +0000

    test STI map & character

--- app/models/gaming_object.rb
+++ app/models/gaming_object.rb
@@ -1,2 +1,3 @@
 class GamingObject < ActiveRecord::Base
+    
 end
--- test/models/gaming_object_test.rb
+++ test/models/gaming_object_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class GamingObjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should have STI map & character" do
+    Map.create(name: "mymap", description: "test")
+    Character.create(name: "mycharacter", description: "test")
+    assert_equal ["mymap"], GamingObject.where(type: "Map").pluck(:name)
+    assert_equal ["mycharacter"], GamingObject.where(type: "Character").pluck(:name)
+  end
+
 end
