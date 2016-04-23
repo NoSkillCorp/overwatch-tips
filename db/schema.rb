2e7dc78e9b45f003d76b2e4fa3c36771f8796eb2
Caillou <igorlacroute@gmail.com>
2016-04-23 15:09:27 +0000

    populate characters & maps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423131334) do
+ActiveRecord::Schema.define(version: 20160423145103) do
 
   create_table "gaming_objects", force: :cascade do |t|
     t.string   "name"
