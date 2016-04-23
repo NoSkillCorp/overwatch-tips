61bb655037a0ac5a6bd6d0f2d12109083d8b21e0
Caillou <igorlacroute@gmail.com>
2016-04-23 12:20:11 +0000

    tip has one map

--- test/models/tip_test.rb
+++ test/models/tip_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has_one map" do
+    map = Map.create(name: "mymap")
+    tip = Tip.create(title:"tip", description: "description")
+    tip.build_tip_map_relation(category: "defense", map: map).save
+    
+    assert_equal "mymap", tip.map.name
+  end
 end
