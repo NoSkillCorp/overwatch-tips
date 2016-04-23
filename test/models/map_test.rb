cf69b7675e6eeb5ee4d5d532b55f0f48c43cf3a8
Caillou <igorlacroute@gmail.com>
2016-04-23 11:57:32 +0000

    test map has_many tips + font

--- test/models/map_test.rb
+++ test/models/map_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class MapTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has_many tips" do
+    map = Map.create(name: "mymap")
+    map.tips.build(description: "tip1", title: "title1")
+    map.tips.build(description: "tip2", title: "title2")
+    assert_equal ["tip1", "tip2"], map.tips.map(&:description)
+  end
 end
