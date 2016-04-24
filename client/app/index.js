7f988bbfbfeca8644d59decc94dfcffd1df8474d
PierreGUI <sabbator@gmail.com>
2016-04-24 21:52:36 +0000

    Bootstrap index.js React+Redux with DevTools

---
+++ client/app/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import ReactOnRails from 'react-on-rails'
+
+ReactOnRails.registerStore({
+  ReduxStore: (props, railsContext) => {
+    console.debug("Setting up Redux store")
+    return createStore({}, {},
+      window.devToolsExtension ? window.devToolsExtension() : undefined
+    )
+  }
+})
+
+ReactOnRails.register({
+  App: () => {
+    console.debug("Setting up React app")
+    return (
+      <Provider store={ReactOnRails.getStore("ReduxStore")}>
+        {/* <-- INSERT APP HERE -!> */}
+      </Provider>
+    )
+  }
+})
