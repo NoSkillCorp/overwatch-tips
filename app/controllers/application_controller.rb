e0881a4d2ffac48591dc6fec84777aeecb8a31d4
Caillou <igorlacroute@gmail.com>
2016-04-24 10:30:13 +0000

    random user_id stored in cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :generate_user_cookie
+  
+  private
+  
+    def generate_user_cookie
+      cookies["user_id"] = SecureRandom.hex unless cookies.has_key?("user_id")
+    end
 end
