e018f485bf18391376adeb9e0bc6125845ed8cb3
Caillou <igorlacroute@gmail.com>
2016-04-24 13:25:38 +0000

    tri des tips par score

--- app/models/gaming_object.rb
+++ app/models/gaming_object.rb
@@ -1,7 +1,10 @@
 class GamingObject < ActiveRecord::Base
     has_many :tips
     
+    #hash with category as keys, and a sorted tips array as value.
     def tips_hash
-        Tip.empty_categories_hash.merge(tips.to_a.group_by(&:category))
+        Tip.empty_categories_hash.merge(tips.to_a.group_by(&:category)).map{|category, tips|
+            [category, tips.sort_by{ |tip| -1*tip.score }]
+        }.to_h
     end
 end
