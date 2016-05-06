86cedfe8f89151867ae138de7153cf03c499ef05
Caillou <igorlacroute@gmail.com>
2016-05-06 16:09:54 +0000

    Add ~50 tips in a migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505145015) do
+ActiveRecord::Schema.define(version: 20160506153955) do
 
   create_table "gaming_objects", force: :cascade do |t|
     t.string   "name"
