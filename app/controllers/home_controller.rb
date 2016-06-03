6c435e051e2d93e8dc688ad3395875a20d5d21cb
Caillou <igorlacroute@gmail.com>
2016-06-03 16:28:37 +0000

    hot page with three categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-      greatest_tips = Tip.ordered_by_score.limit(5)
-      latest_tips = Tip.order(created_at: :desc).limit(5)
-      trending_tips = Tip.ordered_by_vote_count.limit(5).to_a
+      greatest_tips = Tip.greatest_tips(5)
+      latest_tips = Tip.latest_tips(5)
+      trending_tips = Tip.trending_tips(5)
       
       @tips_hash = { greatest: greatest_tips, trending: trending_tips, latest: latest_tips }
       
@@ -10,7 +10,8 @@ class HomeController < ApplicationController
   end
   
   def news
-    @tips = Tip.order(created_at: :desc).page(params[:page]).per(10)
+    @order = params[:order] || "trending"
+    @tips = Tip.send("#{@order}_tips", 10, params[:page])
   end
   
   def about
