036e7414bcf7d64bd9c90740b185a4f5b15594da
Caillou <igorlacroute@gmail.com>
2016-06-03 14:11:50 +0000

    in-text links for all gaming objects

--- app/helpers/tips_helper.rb
+++ app/helpers/tips_helper.rb
@@ -7,4 +7,19 @@ module TipsHelper
             "Playing #{category} in #{gaming_object.name}"
         end
     end
+    
+    def description_with_links(description)
+        gaming_objects = GamingObject.all.to_a
+        words = description.split(" ")
+        words.map{ |word|
+            go = gaming_objects.find{|g| g.name.downcase == word.downcase}
+            if go.present?
+                path = send("#{go.class.to_s.underscore}_path", go)
+                link_to(word, path, class: "in-text-link")
+            else
+                word
+            end
+        }.join(" ")
+        
+    end
 end
