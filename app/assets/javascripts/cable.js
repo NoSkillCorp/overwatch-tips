fda9487741f50ff6a6b8293cb2e7435befeeb358
PierreGUI <sabbator@gmail.com>
2016-06-19 01:17:55 +0000

    Remove old actioncable js

--- app/assets/javascripts/cable.js
+++
@@ -1,9 +0,0 @@
-//= require vendor/actioncable
-//= require_self
-//= require_tree ./channels
-
-(function() {
-  this.App || (this.App = {});
-
-  App.cable = ActionCable.createConsumer('/cable');
-}).call(this);
--- app/assets/javascripts/channels/counter.js
+++
@@ -1,8 +0,0 @@
-App.counter = App.cable.subscriptions.create('CounterChannel', {  
-  received: function(data) {
-    console.log(data)
-    $('#tips_count').html(data.tips);
-    $('#votes_count').html(data.votes);
-    return 
-  }
-});
