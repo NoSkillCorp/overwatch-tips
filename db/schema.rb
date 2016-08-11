4d93b14cf841c3b850c7ccd041556d43c25794fa
Caillou <igorlacroute@gmail.com>
2016-08-11 15:24:03 +0000

    Add Ana's counters

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811135811) do
+ActiveRecord::Schema.define(version: 20160811151632) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
