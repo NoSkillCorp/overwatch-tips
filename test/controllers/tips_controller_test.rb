862b17e7aca57199d9fa40b9283974492ddb84b8
Caillou <igorlacroute@gmail.com>
2016-08-15 14:52:38 +0000

    #Dummy request to contempt the lord of cookies, may his reign last long

--- test/controllers/tips_controller_test.rb
+++ test/controllers/tips_controller_test.rb
@@ -15,11 +15,10 @@ class TipsControllerTest < ActionController::TestCase
     user = User.create
     tip = Tip.create(description: "bla", category: "as", gaming_object: @character, user: user)
     assert_equal "bla", tip.description
-    
+
     cookies["user_id"] = user.user_cookie
     post :update, params: { id: tip.id, tip: { description: "ratata"} }
     assert_equal "ratata", Tip.find(tip.id).description
-    
   end
   
   test "should upvote tip" do
@@ -49,23 +48,29 @@ class TipsControllerTest < ActionController::TestCase
     user = User.create
     tip = Tip.create(description: "bla", category: "as", gaming_object: @character, user: user)
     assert_equal 0, tip.score
-    
+
     cookies["user_id"] = user.user_cookie
-    ap "downvoter 1 : #{user.user_cookie}"
     post :downvote, params: { id: tip.id }
     assert_equal (-1), tip.score
     
     user2 = User.create
+    delete_cookies
     cookies["user_id"] = user2.user_cookie
-    ap "downvoter 2 : #{user2.user_cookie}"
     post :downvote, params: { id: tip.id }
     assert_equal (-2), Tip.find(tip.id).score
     
     user3 = User.create
+    delete_cookies
     cookies["user_id"] = user3.user_cookie
-    ap "downvoter 3 : #{user3.user_cookie}"
     post :upvote, params: { id: tip.id }
     assert_equal (-1), Tip.find(tip.id).score
+  end
+  
+  def delete_cookies
+    #delete cookie "user_id"
+    cookies.delete("user_id")
+    #Dummy request to contempt The Lord of Cookies, may his reign last long
+    post :create, params: { tip: { description: "test", gaming_object_id: @character.id, category: "as" } }
   end
   
 end
