bb1609a71180d43792b24ad8ce03cf067019e754
Caillou <igorlacroute@gmail.com>
2016-08-18 17:59:04 +0000

    API : fix bad request for tips in pgsql

--- app/controllers/api/tips_controller.rb
+++ app/controllers/api/tips_controller.rb
@@ -6,17 +6,14 @@ class Api::TipsController < ApplicationController
     end
     
     def index
-        type = tips_params[:type]
-        gaming_object_id = tips_params[:character_id] || tips_params[:map_id]
+        #type = tips_params[:type]
         category = tips_params[:category]
+        gaming_object_id = tips_params[:character_id] || tips_params[:map_id]
+        gaming_object = GamingObject.friendly.find(gaming_object_id)
         
         #gets the tips of the gaming object, ordered by score and with positive score
-        categotips = category.present? ? Tip.where(category: category) : Tip
-        tips = categotips
-            .joins(:gaming_object)
-            .where("gaming_objects.type = ? AND (gaming_objects.id = ? OR gaming_objects.slug = ?)", type, gaming_object_id, gaming_object_id)
-            .ordered_by_score
-            .select{|tip| tip.score > 0 } #All these calls to the tip.score are redundant with ordered_by_score, could optimize this
+        categotips = category.present? ? gaming_object.tips.where(category: category) : gaming_object.tips
+        tips = categotips.ordered_by_score.select{|tip| tip.score > 0 } #All these calls to the tip.score are redundant with ordered_by_score, could optimize this
             
         render json: tips
     end
