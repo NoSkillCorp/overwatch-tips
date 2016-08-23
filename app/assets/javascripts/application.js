1cfa33cca39c5d46d24a8dccdb6ccaf579905f35
Caillou <igorlacroute@gmail.com>
2016-08-23 15:05:31 +0000

    styling notcie & alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require_tree .
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 
 $(document).ready(function() {
+  
+  $(".alert").on('click', function(){
+    $(this).remove();
+  });
   
   //A Click on any part of a tip redirects to the show of this tip
   function clickTip(element){
