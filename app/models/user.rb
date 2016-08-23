7fd13430e53c778d7f6aad1f5a21fef25ef94284
Caillou <igorlacroute@gmail.com>
2016-08-23 16:00:15 +0000

    creates a folder validators for the RegisteredUserValidator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,3 @@
-class RegistredUserValidator < ActiveModel::Validator
-  def validate(record)
-    #if user is registred (aka if you identify by devise instead of a just a cookie)
-    if record.is_registered || !record.email.nil?
-        #Check email
-        if record.email.blank?
-            record.errors.add(:email, "is missing")
-        elsif record.email !~ Devise.email_regexp
-            record.errors.add(:email, :invalid)
-        elsif User.find_by(is_registered: true, email: record.email).present?
-            record.errors.add(:email, "is already used")
-        end
-        #Check password
-        if record.password.blank?
-            record.errors.add(:password, "is missing")
-        elsif !record.password.length.in?(Devise.password_length)
-            record.errors.add(:password, :invalid, message: "should be between #{Devise.password_length.begin} and #{Devise.password_length.end} characters")
-        elsif record.password != record.password_confirmation
-            record.errors.add(:password_confirmation, :invalid)
-        end
-    end
-  end
-end
-
 class User < ActiveRecord::Base
     # Include default devise modules. Others available are:
     # :recoverable, :confirmable, :lockable, :timeoutable and :omniauthable
@@ -30,14 +6,26 @@ class User < ActiveRecord::Base
     has_many :votes, foreign_key: :user_cookie, primary_key: :user_cookie
     has_many :tips, foreign_key: :user_cookie, primary_key: :user_cookie
     
-    before_save :assign_user_cookie
     validates :user_cookie, uniqueness: true
-    validates_with RegistredUserValidator
+    validates_with RegisteredUserValidator #validates email & password if user is registered or if email is present
     
+    before_save :assign_user_cookie, :set_is_registered
+    
+    
+    #if we are about to save it for the first time and the email is not nil
+    def set_is_registered
+        if !self.persisted? && !self.email.nil?
+            self.is_registered = true
+        end
+        return
+    end
+    
+    #assigns an unused user_cookie
     def assign_user_cookie
        self.user_cookie = User.generate_new_user_cookie if self.user_cookie.blank?
     end
     
+    #generates an aunused user_cookie
     def self.generate_new_user_cookie
         user_cookie = SecureRandom.hex
         user_cookie = User.generate_new_user_cookie if User.find_by(user_cookie: user_cookie).present?
---
+++ app/validators/registered_user_validator.rb
@@ -0,0 +1,23 @@
+class RegisteredUserValidator < ActiveModel::Validator
+  def validate(record)
+    #if user is registred (aka if you identify by devise instead of a just a cookie)
+    if record.is_registered || !record.email.nil?
+        #Check email
+        if record.email.blank?
+            record.errors.add(:email, "is missing")
+        elsif record.email !~ Devise.email_regexp
+            record.errors.add(:email, :invalid)
+        elsif User.find_by(is_registered: true, email: record.email).present?
+            record.errors.add(:email, "is already used")
+        end
+        #Check password
+        if record.password.blank?
+            record.errors.add(:password, "is missing")
+        elsif !record.password.length.in?(Devise.password_length)
+            record.errors.add(:password, :invalid, message: "should be between #{Devise.password_length.begin} and #{Devise.password_length.end} characters")
+        elsif record.password != record.password_confirmation
+            record.errors.add(:password_confirmation, :invalid)
+        end
+    end
+  end
+end
