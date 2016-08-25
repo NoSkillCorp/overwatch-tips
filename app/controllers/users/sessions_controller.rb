358c7f30867fed2418c803fce0ee457fb30597f0
Caillou <igorlacroute@gmail.com>
2016-08-25 15:26:03 +0000

    Re-creates the new action with @new_user instead of @user, to prevent confilct with the @user set by the application

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,12 @@ class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  # Re-creates the new action with @new_user instead of @user, to prevent confilct with the @user set by the application
+   def new
+      @new_user = User.new(sign_in_params)
+      clean_up_passwords(resource)
+      respond_with(@new_user, serialize_options(@new_user))
+   end
 
   # POST /resource/sign_in
   # def create
